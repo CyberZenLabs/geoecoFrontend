4117245dea4a2177bcd9cbe32395c0b3703bcb74
Кирилл Дроздов <kirilldrozdov@MacBook-Pro-Kirill.local>
2022-05-03 13:59:16 +0700

    fix branch

--- EcoCraftsCityReact/src/index.js
+++ EcoCraftsCityReact/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import {
-  BrowserRouter,
-  Routes,
-  Route
+    BrowserRouter,
+    Routes,
+    Route
 } from "react-router-dom"
 import Home from './pages/Home';
 import NavBar from "./components/NavBar"
@@ -13,24 +13,25 @@ import Cart from './pages/Cart';
 import AdminLogin from './pages/AdminLogin';
 import ProductDetail from './pages/ProductDetail';
 import AdminDashboard from './pages/AdminDashboard';
+import AdminForm from "./pages/AdminForm";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-    <NavBar/>
-  
-    <Routes>
-    
-      <Route index element={<Home/>}/>
-      <Route path="cart" element={<Cart/>}/>
-      <Route path="admin-dashboard" element={<AdminDashboard/>}/>
-      <Route path="admin-portal" element={<AdminLogin/>}/>
-      <Route path="product:id" element={<ProductDetail/>}/>
-    </Routes>
-      
-    </BrowserRouter>
-    
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <BrowserRouter>
+            <NavBar/>
 
+            <Routes>
+
+                <Route index element={<Home/>}/>
+                <Route path="cart" element={<Cart/>}/>
+                <Route path="admin-dashboard" element={<AdminDashboard/>}/>
+                <Route path="admin-dashboard/admin-form" element={<AdminForm/>}/>
+                <Route path="admin-portal" element={<AdminLogin/>}/>
+                <Route path="product:id" element={<ProductDetail/>}/>
+            </Routes>
+
+        </BrowserRouter>
+
+    </React.StrictMode>
+);

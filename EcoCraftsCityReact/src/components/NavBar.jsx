1f8d8645af073f96828f1cf02a23875da4fa0438
hacker <hacker@kali>
2022-05-25 22:22:33 +0800

    Hello

--- EcoCraftsCityReact/src/components/NavBar.jsx
+++ EcoCraftsCityReact/src/components/NavBar.jsx
@@ -1,139 +1,117 @@
-import * as React from 'react';
+import * as React from "react";
 import { Link } from "react-router-dom";
-import image from '../img/logo.svg'
+import image from "../img/logo.svg";
 import styled from "styled-components/macro";
-import UilListUl from '@iconscout/react-unicons/icons/uil-list-ul';
-import UilSearch from '@iconscout/react-unicons/icons/uil-search';
-import UilUser from '@iconscout/react-unicons/icons/uil-user';
-import UilShoppingCart from '@iconscout/react-unicons/icons/uil-shopping-cart';
+import UilListUl from "@iconscout/react-unicons/icons/uil-list-ul";
+import UilSearch from "@iconscout/react-unicons/icons/uil-search";
+import UilUser from "@iconscout/react-unicons/icons/uil-user";
+import UilShoppingCart from "@iconscout/react-unicons/icons/uil-shopping-cart";
 import {
-    DivBoxLogoSC,
-    DivBoxHeaderSC,
-    DivHeaderSC,
-    DivCatalogAndSearchBoxSC,
-    SpanFirstSC,
-    SpanSecondSC,
-    DivTextBoxSC,
-    DivBoxButtonAndInputSC,
-    DivBoxIconHeaderSC,
-    DivBoxIconSC,
-    ButtonSC,
-    BoxContentButton,
-    InputSC,
-    DivInputBoxCS,
-    DivIconBoxInput,
-    LinkIconSC,
-    DivBoxIconEndSC,
-    SpanEndHeaderSC
-} from '../styled-components-css/styles.navbar'
-import {ButtonCustomSC} from "../styled-components-css/styles.custom-button";
+  DivBoxLogoSC,
+  DivBoxHeaderSC,
+  DivHeaderSC,
+  DivCatalogAndSearchBoxSC,
+  SpanFirstSC,
+  SpanSecondSC,
+  DivTextBoxSC,
+  DivBoxButtonAndInputSC,
+  DivBoxIconHeaderSC,
+  DivBoxIconSC,
+  ButtonSC,
+  BoxContentButton,
+  InputSC,
+  DivInputBoxCS,
+  DivIconBoxInput,
+  LinkIconSC,
+  DivBoxIconEndSC,
+  SpanEndHeaderSC,
+} from "../styled-components-css/styles.navbar";
+import { ButtonCustomSC } from "../styled-components-css/styles.custom-button";
+import { OverlayDivSc } from "../styled-components-css/styles.catalog";
+import AppContext from "../context/AppContext";
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const ResponsiveAppBar = () => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    return (
-        <DivBoxHeaderSC>
-            <DivHeaderSC>
-                <DivBoxLogoSC>
-                </DivBoxLogoSC>
+  const { setShowCatalog, showCatalog } = React.useContext(AppContext);
+  return (
+    <DivBoxHeaderSC>
+      <DivHeaderSC>
+        <DivBoxLogoSC></DivBoxLogoSC>
 
-                <DivCatalogAndSearchBoxSC>
-                    <DivTextBoxSC>
-                        <SpanFirstSC to="/">
-                            Продавайте на EcoCraftCity
-                        </SpanFirstSC>
-                        <SpanSecondSC to="/">
-                            Работа в EcoCraftCity
-                        </SpanSecondSC>
-                    </DivTextBoxSC>
-                    <DivBoxButtonAndInputSC>
-                        <ButtonCustomSC
-                            width={'176px'}
-                            padding={'8px 32px'}
-                        >
-                            <BoxContentButton
+        <DivCatalogAndSearchBoxSC>
+          <DivTextBoxSC>
+            <SpanFirstSC to="/">Продавайте на EcoCraftCity</SpanFirstSC>
+            <SpanSecondSC to="/">Работа в EcoCraftCity</SpanSecondSC>
+          </DivTextBoxSC>
+          <DivBoxButtonAndInputSC>
+            <ButtonCustomSC
+              onClick={() => setShowCatalog(!showCatalog)}
+              width={"176px"}
+              padding={"8px 32px"}
+            >
+              <BoxContentButton>
+                <UilListUl
+                  size="40"
+                  // color="#61DAFB"
+                />
+                <span>Каталог</span>
+              </BoxContentButton>
+            </ButtonCustomSC>
 
-                            >
-                                <UilListUl
-                                    size="40"
-                                    // color="#61DAFB"
-                                />
-                                <span>
-                                    Каталог
-                                </span>
+            <DivInputBoxCS>
+              <InputSC
+                // value={lastName}
+                // onChange={onChange(getLastName)}
+                type="text"
+                id={"search"}
+                placeholder={"Я ищу..."}
+              />
+              <DivIconBoxInput>
+                <UilSearch size="25" color="rgba(37, 37, 37, 0.7)" />
+              </DivIconBoxInput>
+            </DivInputBoxCS>
+          </DivBoxButtonAndInputSC>
+        </DivCatalogAndSearchBoxSC>
+        <DivBoxIconHeaderSC>
+          <DivBoxIconSC>
+            <LinkIconSC to="/">
+              <DivBoxIconEndSC>
+                <UilUser size="35" color="rgba(37, 37, 37, 0.8)" />
+              </DivBoxIconEndSC>
 
-                            </BoxContentButton>
-                        </ButtonCustomSC>
+              <SpanEndHeaderSC>Войти</SpanEndHeaderSC>
+            </LinkIconSC>
+            <LinkIconSC to="/">
+              <DivBoxIconEndSC>
+                <UilShoppingCart size="35" color="rgba(37, 37, 37, 0.8)" />
+              </DivBoxIconEndSC>
 
-                        <DivInputBoxCS>
-                            <InputSC
-                                // value={lastName}
-                                // onChange={onChange(getLastName)}
-                                type="text"
-                                id={"search"}
-                                placeholder={"Я ищу..."}
-                            />
-                            <DivIconBoxInput>
-                                <UilSearch
-                                    size="25"
-                                    color="rgba(37, 37, 37, 0.7)"
-                                />
-                            </DivIconBoxInput>
-                        </DivInputBoxCS>
-
-                    </DivBoxButtonAndInputSC>
-
-                </DivCatalogAndSearchBoxSC>
-                <DivBoxIconHeaderSC>
-
-                    <DivBoxIconSC>
-                        <LinkIconSC to="/">
-                            <DivBoxIconEndSC>
-                                <UilUser
-                                    size="35"
-                                    color="rgba(37, 37, 37, 0.8)"
-                                />
-                            </DivBoxIconEndSC>
-
-                            <SpanEndHeaderSC>
-                                Войти
-                            </SpanEndHeaderSC>
-                        </LinkIconSC>
-                        <LinkIconSC to="/">
-                            <DivBoxIconEndSC>
-                                <UilShoppingCart
-                                    size="35"
-                                    color="rgba(37, 37, 37, 0.8)"
-                                />
-                            </DivBoxIconEndSC>
-
-                            <SpanEndHeaderSC>
-                                Корзина
-                            </SpanEndHeaderSC>
-                        </LinkIconSC>
-                    </DivBoxIconSC>
-                </DivBoxIconHeaderSC>
-            </DivHeaderSC>
-        </DivBoxHeaderSC>
-    );
+              <SpanEndHeaderSC>Корзина</SpanEndHeaderSC>
+            </LinkIconSC>
+          </DivBoxIconSC>
+        </DivBoxIconHeaderSC>
+      </DivHeaderSC>
+    </DivBoxHeaderSC>
+  );
 };
 
 export default ResponsiveAppBar;
--- EcoCraftsCityReact/src/index.js
+++ EcoCraftsCityReact/src/index.js
@@ -1,41 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./page-css/sidenav.css";
 
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom"
-import Home from './pages/Home';
-import NavBar from "./components/NavBar"
-import Cart from './pages/Cart';
-import AdminLogin from './pages/AdminLogin';
-import ProductDetail from './pages/ProductDetail';
-import AdminDashboard from './pages/AdminDashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Cart from "./pages/Cart";
+import AdminLogin from "./pages/AdminLogin";
+import ProductDetail from "./pages/ProductDetail";
+import AdminDashboard from "./pages/AdminDashboard";
 import AdminForm from "./pages/AdminForm";
 import Registration from "./pages/Registration";
 import Footer from "./components/Footer";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Catalog from "./components/Catalog";
+import { AppProvider } from "./context/AppContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <NavBar/>
+  <React.StrictMode>
+    <AppProvider>
+      <BrowserRouter>
+        <div id="overlay-nav"></div>
+        <NavBar />
+        <Catalog />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="admin-dashboard" element={<AdminDashboard />} />
+          <Route path="admin-dashboard/admin-form" element={<AdminForm />} />
+          <Route path="registration" element={<Registration />} />
+          <Route path="admin-portal" element={<AdminLogin />} />
+          <Route path="product/:id" element={<ProductDetail />} />
+        </Routes>
 
-            <Routes>
-
-                <Route index element={<Home/>}/>
-                <Route path="cart" element={<Cart/>}/>
-                <Route path="admin-dashboard" element={<AdminDashboard/>}/>
-                <Route path="admin-dashboard/admin-form" element={<AdminForm/>}/>
-                <Route path="registration" element={<Registration/>}/>
-                <Route path="admin-portal" element={<AdminLogin/>}/>
-                <Route path="product/:id" element={<ProductDetail/>}/>
-            </Routes>
-
-            <Footer/>
-        </BrowserRouter>
-
-    </React.StrictMode>
+        <Footer />
+      </BrowserRouter>
+    </AppProvider>
+  </React.StrictMode>
 );

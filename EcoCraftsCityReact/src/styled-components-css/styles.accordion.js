bb1343d7badbe301b80ddc5d1bbf2cab293cb316
hacker <hacker@kali>
2022-05-25 15:07:25 +0800

    arrow response

--- EcoCraftsCityReact/src/styled-components-css/styles.accordion.js
+++ EcoCraftsCityReact/src/styled-components-css/styles.accordion.js
@@ -1,26 +1,31 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const AccordionImage = styled.img`
+  width: 100%;
+  max-width: 1200px;
+  max-height: 400px;
 
-width: auto;
-max-width: 1200px;
-max-height: 400px;
-
-
-border-radius: 10px;
-
-`
+  border-radius: 10px;
+`;
 
 export const Slider = styled.div`
-    position: relative;
-    height:400px;
-    display: flex;
-    margin:100px 0;
-    justify-content: center;
-    align-items: center;
-
-
-`
-
-
-
+  position: relative;
+  max-width: 1296px;
+  width: 80%;
+  /* height: 110%; */
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  /* margin: 100px 0; */
+  margin: auto;
+  padding: 6% 0;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`;

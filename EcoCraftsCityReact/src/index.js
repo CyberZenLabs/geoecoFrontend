912820de3299f22865e0c1d991e8849cef3f8ca7
Kirdro <kirdro@yandex.ru>
2022-05-27 20:29:52 +0700

    fix emp

--- EcoCraftsCityReact/src/index.js
+++ EcoCraftsCityReact/src/index.js
@@ -18,25 +18,25 @@ import { AppProvider } from "./context/AppContext";
 import SignIn from "./pages/SignIn";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AppProvider>
-      <BrowserRouter>
-        <div id="overlay-nav"></div>
-        <NavBar />
-        <Catalog />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="admin-dashboard" element={<AdminDashboard />} />
-          <Route path="admin-dashboard/admin-form" element={<AdminForm />} />
-          <Route path="registration" element={<Registration />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="admin-portal" element={<AdminLogin />} />
-          <Route path="product/:id" element={<ProductDetail />} />
-        </Routes>
+    <React.StrictMode>
+        <AppProvider>
+            <BrowserRouter>
+                <div id="overlay-nav"></div>
+                <NavBar />
+                <Catalog />
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="cart" element={<Cart />} />
+                    <Route path="admin-dashboard" element={<AdminDashboard />} />
+                    <Route path="admin-dashboard/admin-form" element={<AdminForm />} />
+                    <Route path="registration" element={<Registration />} />
+                    <Route path="signin" element={<SignIn />} />
+                    <Route path="admin-portal" element={<AdminLogin />} />
+                    <Route path="product/:id" element={<ProductDetail />} />
+                </Routes>
 
-        <Footer />
-      </BrowserRouter>
-    </AppProvider>
-  </React.StrictMode>
+                <Footer />
+            </BrowserRouter>
+        </AppProvider>
+    </React.StrictMode>
 );

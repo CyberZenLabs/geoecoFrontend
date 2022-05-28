2e8c21fb960d4e50d568dfb50e32a490ed48b488
hacker <hacker@kali>
2022-05-28 20:46:50 +0800

    profile

--- EcoCraftsCityReact/src/components/NavBar.jsx
+++ EcoCraftsCityReact/src/components/NavBar.jsx
@@ -14,7 +14,6 @@ import {
   DivBoxButtonAndInputSC,
   DivBoxIconHeaderSC,
   DivBoxIconSC,
- 
   BoxContentButton,
   InputSC,
   DivInputBoxCS,
@@ -26,12 +25,19 @@ import {
 import { ButtonCustomSC } from "../styled-components-css/styles.custom-button";
 
 import AppContext from "../context/AppContext";
+import {
+  DivAccountWrapperSc,
+  DivStyledProfileSc,
+} from "../styled-components-css/styles.profile";
+import { useAuth } from "../context/AuthContext";
 
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [profileActive, setProfileActive] = React.useState(false);
+  const { login, logout, loggedIn, setFormValues, authError } = useAuth();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -48,6 +54,7 @@ const ResponsiveAppBar = () => {
     setAnchorElUser(null);
   };
 
+  const toggleProfile = () => {};
   const { setShowCatalog, showCatalog } = React.useContext(AppContext);
   return (
     <DivBoxHeaderSC>
@@ -90,13 +97,50 @@ const ResponsiveAppBar = () => {
         </DivCatalogAndSearchBoxSC>
         <DivBoxIconHeaderSC>
           <DivBoxIconSC>
-            <LinkIconSC to="/signin">
-              <DivBoxIconEndSC>
-                <UilUser size="35" color="rgba(37, 37, 37, 0.8)" />
-              </DivBoxIconEndSC>
+            <DivAccountWrapperSc>
+              {loggedIn ? (
+                <div class="container">
+                  <div class={profileActive ? "navigation active" : "active"}>
+                    <div class="user-box">
+                      <div class="image-box">
+                        <img
+                          src="https://i.pravatar.cc/150?img=49"
+                          alt="avatar"
+                        />
+                      </div>
+                      <p class="username">Jenifer Lopez</p>
+                    </div>
+                    <div class="menu-toggle" onClick={toggleProfile}></div>
+                    <ul class="menu">
+                      <li>
+                        <a href="#">Profile</a>
+                      </li>
+                      <li>
+                        <a href="#">Messages</a>
+                      </li>
+                      <li>
+                        <a href="#">Notification</a>
+                      </li>
+                      <li>
+                        <a href="#">Settings</a>
+                      </li>
+                      <li>
+                        <a href="#">Logout</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              ) : (
+                <LinkIconSC to="/signin">
+                  <DivBoxIconEndSC>
+                    <UilUser size="35" color="rgba(37, 37, 37, 0.8)" />
+                  </DivBoxIconEndSC>
 
-              <SpanEndHeaderSC>Войти</SpanEndHeaderSC>
-            </LinkIconSC>
+                  <SpanEndHeaderSC>Войти</SpanEndHeaderSC>
+                </LinkIconSC>
+              )}
+            </DivAccountWrapperSc>
+
             <LinkIconSC to="/">
               <DivBoxIconEndSC>
                 <UilShoppingCart size="35" color="rgba(37, 37, 37, 0.8)" />

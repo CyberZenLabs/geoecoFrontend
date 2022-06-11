2fba911d868af55103bf014804680c97af9647cb
hacker <hacker@kali>
2022-06-11 14:11:08 +0700

    didit

--- EcoCraftsCityReact/src/pages/Home.jsx
+++ EcoCraftsCityReact/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import useAxiosFunction from "../hooks/useAxiosFunction";
 import Carousel from "../components/Carousel";
 import ProductCard from "../components/ProductCard";
 import StarRating from "../components/StarRating";
@@ -8,17 +8,39 @@ import {
   HomeContainer,
 } from "../styled-components-css/styles.product-card";
 import { SliderData } from "../utils/sliderData";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [response, error, loading, axiosFetch] = useAxiosFunction();
+  useEffect(() => {
+    // axiosFetch({
+    //   axiosInstance: axios,
+    //   auth: "",
+    //   method: "GET",
+    //   url: `https://radiant-river-29802.herokuapp.com/api/v1/products`,
+    //   requestConfig: {},
+    // });
+    // setProducts([response]);
+
+    axios
+      .get("https://radiant-river-29802.herokuapp.com/api/v1/products")
+      .then((res) => {
+        setProducts(res.data.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <HomeContainer>
         <Carousel slides={SliderData} />
-       
-          {[...Array(15)].map((product) => {
-            return <ProductCard />;
-          })}
-     
+
+        {products.map((product) => {
+          return <ProductCard product={product} />;
+        })}
       </HomeContainer>
     </>
   );

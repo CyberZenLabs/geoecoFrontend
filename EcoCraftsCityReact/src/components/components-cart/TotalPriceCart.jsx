3b6f232c57ea31191afebc670183c9a12fb96abb
nub24 <nub24@rambler.ru>
2022-07-12 20:12:17 +0700

    111

--- EcoCraftsCityReact/src/components/components-cart/TotalPriceCart.jsx
+++ EcoCraftsCityReact/src/components/components-cart/TotalPriceCart.jsx
@@ -1,12 +1,78 @@
 import React from 'react';
-import {DivBoxTotalPriceSC} from "../../styled-components-css/styles.cart";
+import { MdInfoOutline } from 'react-icons/md';
+import { FiArrowRight } from 'react-icons/fi';
+import { ButtonCustomSC } from '../../styled-components-css/styles.custom-button';
+import { DivBoxTotalPriceSC } from '../../styled-components-css/styles.cart';
+import {
+	DivMainTotalboxSC,
+	DivTotalTitleSC,
+	DivTotalGoodsSC,
+	DivTotalSaleSC,
+	DivTotalDeliveryDescSC,
+	DivTotalDeliveryTitleSC,
+	DivTotalDeliveryAddressSC,
+	DivTotalDateTitleSC,
+	DivTotalDateSC,
+	DivTotalPaymentTitleSC,
+	DivTotalPaymentSC,
+	DivTotalConditionsSC,
+} from '../../styled-components-css/styles.price-cart';
 
-const TotalPriceCart = (props) => {
-    return (
-        <DivBoxTotalPriceSC>
+const TotalPriceCart = props => {
+	return (
+		<DivBoxTotalPriceSC>
+			<DivMainTotalboxSC>
+				<DivTotalTitleSC>
+					<h2>Итого</h2>
+					<h2>15000</h2>
+				</DivTotalTitleSC>
+				<DivTotalGoodsSC>
+					<p>Товары, 5 шт.</p>
+					<p>123126 р.</p>
+				</DivTotalGoodsSC>
+				<DivTotalSaleSC>
+					<p>Скидка</p>
+					<p>-44568 р.</p>
+				</DivTotalSaleSC>
+				<DivTotalDeliveryDescSC>
+					<p>Доставка</p>
+					<p>Бесплатно</p>
+				</DivTotalDeliveryDescSC>
+				<DivTotalDeliveryTitleSC>
+					<h3>Доставка:</h3>
+				</DivTotalDeliveryTitleSC>
+				<DivTotalDeliveryAddressSC>
+					<p>Адрес</p>
+				</DivTotalDeliveryAddressSC>
+				<DivTotalDateTitleSC>
+					<p>Дата:</p>
+				</DivTotalDateTitleSC>
+				<DivTotalDateSC>
+					<p>дд/мм</p>
+				</DivTotalDateSC>
+				<DivTotalPaymentTitleSC>
+					<p>Оплата:</p>
+				</DivTotalPaymentTitleSC>
+				<DivTotalPaymentSC>
+					<p>Карта</p>
+				</DivTotalPaymentSC>
+				<DivTotalConditionsSC>
+					<MdInfoOutline />
+					<p>Условия возврата и доставки</p>
+				</DivTotalConditionsSC>
 
-        </DivBoxTotalPriceSC>
-    );
-}
+				<ButtonCustomSC
+					// onClick={() => setShowCatalog(!showCatalog)}
+					width={'100%'}
+					// padding={'8px 32px'}
+					primary={true}
+				>
+					<span>Оплатить заказ</span>
+					<FiArrowRight />
+				</ButtonCustomSC>
+			</DivMainTotalboxSC>
+		</DivBoxTotalPriceSC>
+	);
+};
 
 export default TotalPriceCart;
---
+++ EcoCraftsCityReact/src/styled-components-css/styles.price-cart.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const DivMainTotalboxSC = styled.div`
+	width: 100%;
+	height: 100%;
+	padding: 82px 50px 19px;
+	margin: 0;
+	display: grid;
+	grid-template-rows: repeat(11, auto);
+	font-family: 'Montserrat';
+`;
+
+export const DivTotalTitleSC = styled.div`
+	display: flex;
+	justify-content: space-between;
+	font-weight: 700;
+	font-size: 30px;
+	color: #252525;
+`;
+
+export const DivTotalGoodsSC = styled.div`
+	display: flex;
+	justify-content: space-between;
+	font-weight: 600;
+	font-size: 20px;
+	color: #252525cc;
+`;
+
+export const DivTotalSaleSC = styled.div`
+	display: flex;
+	justify-content: space-between;
+	font-weight: 600;
+	font-size: 20px;
+	color: #252525cc;
+`;
+
+export const DivTotalDeliveryDescSC = styled.div`
+	display: flex;
+	justify-content: space-between;
+	font-weight: 600;
+	font-size: 20px;
+	color: #252525cc;
+`;
+export const DivTotalDeliveryTitleSC = styled.div`
+	display: flex;
+	font-weight: 700;
+	font-size: 25px;
+	color: #252525;
+`;
+export const DivTotalDeliveryAddressSC = styled.div`
+	font-weight: 600;
+	font-size: 20px;
+	color: #85cb33b2; ;
+`;
+
+export const DivTotalDateTitleSC = styled.div`
+	font-weight: 700;
+	font-size: 25px;
+	color: #252525;
+`;
+
+export const DivTotalDateSC = styled.div`
+	font-weight: 600;
+	font-size: 20px;
+	color: #85cb33b2; ;
+`;
+
+export const DivTotalPaymentTitleSC = styled.div`
+	font-weight: 700;
+	font-size: 25px;
+	color: #252525;
+`;
+
+export const DivTotalPaymentSC = styled.div`
+	font-weight: 600;
+	font-size: 20px;
+	color: #85cb33b2; ;
+`;
+
+export const DivTotalConditionsSC = styled.div`
+	font-weight: 600;
+	font-size: 20px;
+	color: #252525cc;
+`;

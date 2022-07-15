5196ab8d2e95e23fe3efa17d765661b23a9cd415
DMCprogs <dragonwar322@gmail.com>
2022-07-15 19:23:54 +0700

    Delivery menu component created

--- EcoCraftsCityReact/src/pages/Cart.jsx
+++ EcoCraftsCityReact/src/pages/Cart.jsx
@@ -40,9 +40,7 @@ const Cart = () => {
 
                 </TotalPriceCart>
             </DivBoxFirstRowSC>
-           <BoxInfoDelivery>
-            
-           </BoxInfoDelivery>
+           
         </DivContentCartSC>
     </DivCartBoxSC>
   )

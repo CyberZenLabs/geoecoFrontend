8e900657425ff4128b2ef609532732dcb9dfa179
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-07-23 12:48:02 +0700

    рефакторинг странички привязка карты

--- EcoCraftsCityReact/src/pages/MapBinding.jsx
+++ EcoCraftsCityReact/src/pages/MapBinding.jsx
@@ -1,97 +1,84 @@
-import React from 'react'
+import React from 'react';
 import {
-    DivMapBindingBoxSC,
-    DivTextSC,
-    DivButtonsSC,
-    DivContentMapBindingSC,
-    DivBoxNumCardSC,
-    DivRowBoxSC,
-    DivBoxMMSC,
-    DivColumnsBoxSC,
-    DivBoxGGSC,
-    DivBoxCVVCVCSC,
-    DivSlashSC,
-    DivImgBoxSC
-} from "../styled-components-css/styles.map-binding";
-import TextField from "../components/TextField";
-import { ButtonCustomSC } from "../styled-components-css/styles.custom-button";
-import { ButtonCustomWhiteSC } from "../styled-components-css/styles.custom-button-white";
+  DivMapBindingBoxSC,
+  DivTextSC,
+  DivButtonsSC,
+  DivContentMapBindingSC,
+  DivBoxNumCardSC,
+  DivBoxMMSC,
+  DivBoxGGSC,
+  DivBoxCVVCVCSC,
+  DivSlashSC,
+  DivImgBoxSC,
+  DivContainerRowsMB,
+  DivRowBoxSC,
+  DivColumnsBoxSC,
+} from '../styled-components-css/styles.map-binding';
+import TextField from '../components/TextField';
+import { ButtonCustomSC } from '../styled-components-css/styles.custom-button';
+import { ButtonCustomWhiteSC } from '../styled-components-css/styles.custom-button-white';
 import { Formik, Form } from 'formik';
 
 const MapBinding = () => {
-    const startingValues = { text: "" };
-    return (
-        <DivMapBindingBoxSC>
-            <DivContentMapBindingSC>
-                <DivTextSC>Привязка карты</DivTextSC>
-                <Formik initialValues={startingValues}>
-                    {(formik) => (
-                        <Form>
-                            <DivRowBoxSC>
-                                <DivBoxNumCardSC>
-                                    <TextField
-                                        label="cardNumber"
-                                        type="text"
-                                        name="cardNumber"
-                                        fullSize={false}
-                                        placeholder={"Номер карты"}
-                                    />
-                                </DivBoxNumCardSC>
+  const startingValues = { text: '' };
+  return (
+    <DivMapBindingBoxSC>
+      <DivContentMapBindingSC>
+        <DivTextSC>Привязка карты</DivTextSC>
+        <Formik initialValues={startingValues}>
+          {(formik) => (
+            <Form>
+              <DivImgBoxSC>
+                <DivContainerRowsMB>
+                  <DivRowBoxSC>
+                    <DivBoxNumCardSC>
+                      <TextField
+                        label="cardNumber"
+                        type="text"
+                        name="cardNumber"
+                        fullSize={false}
+                        placeholder={'Номер карты'}
+                      />
+                    </DivBoxNumCardSC>
+                  </DivRowBoxSC>
+                  <DivColumnsBoxSC>
+                    <DivBoxMMSC>
+                      <TextField label="month" type="text" name="month" fullSize={false} placeholder={'ММ'} />
+                    </DivBoxMMSC>
 
-                                <DivColumnsBoxSC>
+                    <DivSlashSC>/</DivSlashSC>
 
-                                <DivBoxMMSC>
-                                    <TextField
-                                        label="month"
-                                        type="text"
-                                        name="month"
-                                        fullSize={false}
-                                        placeholder={"ММ"}
-                                    />
-                                </DivBoxMMSC>
+                    <DivBoxGGSC>
+                      <TextField label="year" type="text" name="year" fullSize={false} placeholder={'ГГ'} />
+                    </DivBoxGGSC>
+                    <DivBoxCVVCVCSC>
+                      <TextField label="text" type="text" name="text" fullSize={false} placeholder={'CVV/CVC'} />
+                    </DivBoxCVVCVCSC>
+                  </DivColumnsBoxSC>
+                </DivContainerRowsMB>
+              </DivImgBoxSC>
 
-                                <DivSlashSC>/</DivSlashSC>
+              <DivButtonsSC>
+                <ButtonCustomWhiteSC width={'100%'} padding={'18px 32px'} type="submit">
+                  Отмена
+                </ButtonCustomWhiteSC>
 
-                                <DivBoxGGSC>
-                                    <TextField
-                                        label="year"
-                                        type="text"
-                                        name="year"
-                                        fullSize={false}
-                                        placeholder={"ГГ"}
-                                    />
-                                </DivBoxGGSC>
-                                <DivBoxCVVCVCSC>
-                                    <TextField
-                                        label="text"
-                                        type="text"
-                                        name="text"
-                                        fullSize={false}
-                                        placeholder={"CVV/CVC"}
-                                    />
-                                </DivBoxCVVCVCSC>
-                                </DivColumnsBoxSC>
-                                </DivRowBoxSC>
-                                <DivImgBoxSC></DivImgBoxSC>
-                            <DivButtonsSC>
-                                <ButtonCustomWhiteSC
-                                    width={"100%"}
-                                    padding={"18px 32px"}
-                                    type="submit">Отмена</ButtonCustomWhiteSC>
-
-                                <ButtonCustomSC
-                                disabled={!formik.dirty || !formik.isValid}
-                                statusOpasity={!formik.dirty || !formik.isValid}
-                                    width={"100%"}
-                                    padding={"18px 32px"}
-                                    type="submit">Привязать</ButtonCustomSC>
-                            </DivButtonsSC>
-                        </Form>
-                    )}
-                </Formik>
-            </DivContentMapBindingSC>
-        </DivMapBindingBoxSC >
-    )
-}
+                <ButtonCustomSC
+                  disabled={!formik.dirty || !formik.isValid}
+                  statusOpasity={!formik.dirty || !formik.isValid}
+                  width={'100%'}
+                  padding={'18px 32px'}
+                  type="submit"
+                >
+                  Привязать
+                </ButtonCustomSC>
+              </DivButtonsSC>
+            </Form>
+          )}
+        </Formik>
+      </DivContentMapBindingSC>
+    </DivMapBindingBoxSC>
+  );
+};
 
 export default MapBinding;
--- EcoCraftsCityReact/src/styled-components-css/styles.map-binding.js
+++ EcoCraftsCityReact/src/styled-components-css/styles.map-binding.js
@@ -1,128 +1,109 @@
-import styled from "styled-components/macro"
 /* import image from "../img/white-card1.svg"
 import image2 from "../img/white-card2.svg"
 import image3 from "../img/cards-logo.svg"
 import image4 from "../img/gray-rectangle.svg" */
-import image from "../img/Cards.svg"
-
+import imageMB from '../img/Cards.svg';
+import styled from 'styled-components';
 
 export const DivMapBindingBoxSC = styled.div`
-display: grid;
-justify-items: center;
-`
+  display: grid;
+  justify-items: center;
+`;
 
 export const DivContentMapBindingSC = styled.div`
-display: grid;
-grid-template-rows: 223px 304px 416px;
-max-width: 571px;
-width: 85vw;
-`
+  display: grid;
+  //grid-template-rows: 223px 304px 416px;
+  max-width: 600px;
+  width: 85vw;
+`;
 
- export const DivBoxNumCardSC = styled.div`
- position: absolute;
- width: 355px;
- height: 56px;
- left: 32px;
- right: 201px;
- bottom: 130px;
- top: 118px;
- padding-top: 10px;
- padding-left: 8px;
- 
- `
- export const DivBoxMMSC = styled.div`
- position: absolute;
- width: 86px;
- height: 56px;
- left: 32px;
- right: 367px;
- bottom: 58px;
- top: 190px;
- padding-left: 8px;
- padding-top: 16px;
- `
- 
- export const DivBoxGGSC = styled.div`
- position: absolute;
- width: 86px;
- height: 56px;
- left: 129px;
- right: 367px;
- bottom: 58px;
- top: 190px;
- padding-left: 8px;
- margin-left: 20px;
- padding-top: 16px;
- ::-webkit-input-placeholder{
-  text-align:center;
-}
- `
- export const DivBoxCVVCVCSC = styled.div`
- position: absolute;
- width: 109px;
- height: 56px;
- top: 227px;
- left: 422;
- bottom: 47px;
- right: 30px;
- `
+export const DivContainerRowsMB = styled.div`
+  display: grid;
+  grid-template-rows: 139px 141px;
+  @media (max-width: 686px) {
+    width: 100%;
+  }
+`;
+export const DivRowBoxSC = styled.div`
+  max-width: 355px;
+  width: 100%;
+`;
+
+export const DivBoxNumCardSC = styled.div`
+  margin-top: 70px;
+  margin-left: 38px;
+  width: 355px;
+  height: 56px;
+`;
+
+export const DivColumnsBoxSC = styled.div`
+  display: grid;
+  grid-template-columns: ${({ fullSize }) => (fullSize ? '100%' : '67px 6px 67px 109px')};
+  @media (max-width: 686px) {
+    width: 100%;
+  }
+`;
+
+export const DivBoxMMSC = styled.div`
+  width: 100%;
+  height: 56px;
+  margin-left: 38px;
+`;
+
+export const DivBoxGGSC = styled.div`
+  width: 100%;
+  height: 56px;
+  margin-left: 76px;
+`;
+export const DivBoxCVVCVCSC = styled.div`
+  width: 100%;
+  height: 56px;
+  margin-left: 335px;
+  margin-top: 11px;
+`;
 export const DivSlashSC = styled.div`
-position: absolute;
-top: 208px;
-right: 446px;
-bottom: 76px;
-left: 130px;
-width: 6px;
-height: 20px;
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 500;
-font-size: 16px;
-line-height: 20px;
-text-align: center;
-padding-left: 4px;
-padding-top: 16px;
-color: rgba(37, 37, 37, 0.7);
-`
+  width: 100%;
+  height: 20px;
+  margin-left: 58px;
+  margin-top: 17px;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+  color: rgba(37, 37, 37, 0.7);
+`;
 
-export const DivTextSC = styled.div
- `font-weight: 700;
+export const DivTextSC = styled.div`
+  font-weight: 700;
   font-size: 32px;
   display: inline-block;
   width: fit-content;
   padding-top: 179px;
   padding-left: 24px;
-`
- 
+  @media (max-width: 360px) {
+    font-size: 28px;
+  }
+`;
+
 export const DivImgBoxSC = styled.div`
-background-image: url(${image});
-background-repeat: no-repeat;
-background-size: 600px;
-width: 559px;
-height: 345px;
-
-`
-
-  export const DivButtonsSC = styled.div`
-  display: grid;
-  grid-template-columns: ${({ fullSize }) => (fullSize ? "100%" : "259px 259px")};
-  grid-column-gap: 29px;
-  @media (max-width: 643px) {
-    display: block !important;
+  background-image: url(${imageMB});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  height: 280px;
+  @media (max-width: 730px) {
     width: 100%;
-  } 
-  padding-left: 24px;
-  padding-top: 44px;
-  padding-bottom: 316px;
-  `
+  }
+`;
 
-  export const DivRowBoxSC = styled.div`
+export const DivButtonsSC = styled.div`
   display: grid;
-  grid-template-rows: 36px 21px;
-  position: relative;
- 
-  `
- export const DivColumnsBoxSC = styled.div`
- display: grid;
- grid-template-columns: 67px 6px 67px 109px;
- `
+  /* grid-template-columns: ${({ fullSize }) => (fullSize ? '100%' : '259px 259px')}; */
+  grid-column-gap: 29px;
+  max-width: 676px;
+  width: 100%;
+  padding-left: 24px;
+  padding-bottom: 316px;
+  padding-top: 65px;
+`;

c2b75625dc8e8969b652ab44b502228c22da1015
SvetlanaSob <hino.ray00@gmail.com>
2022-07-25 18:03:29 +0700

    VenderProfile

--- EcoCraftsCityReact/src/pages/VendorProfile.jsx
+++ EcoCraftsCityReact/src/pages/VendorProfile.jsx
@@ -1,62 +1,178 @@
 import { Margin } from '@mui/icons-material';
-import React from 'react';
-import SettingsShop from "../components/SettingsShop";
-import{
-    DivBoxNewProductsST,
-    MenuProductST,
-  GreenST,
-  DivBoxNewProductsBigST,
+import React, { useState } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+
+import useReactRouterBreadcrumbs from 'use-react-router-breadcrumbs';
+import StarRating from '../components/StarRating';
+import { DivBackBoxSC, DivHistorySC, NavLinkSC } from '../styled-components-css/styles.product-detail';
+import {
+  DivOptionsPanelSC,
+  DivStarsPanelSC,
+  DivStoreInfoDataSC,
+  DivStoreInfoImageSC,
+  DivStoreInfoSC,
+  DivStoreInfoStuffSC,
+  DivStoreLeftPanelSC,
+  DivStoreOptionsLeftPanelSC,
+  DivStoreOptionsLinkSC,
+  DivStoreOptionsLinkStoreSC,
+  DivStoreOptionsRightPanelSC,
+  DivStoreOptionsSC,
+  DivStoreRightPanelSC,
+  DivStoreWrapSC,
+  DivWrapLinkSC,
+  H1BoldTextSC,
+  HrLinkSC,
+  ProductsNumSC,
+  StoreInfoHeaderSC,
+  StoreInfoSubHeaderSC,
+  StoreSalesAndAccountLinkSC,
+  StoreSalesAndAccountLinkSearchSC,
+} from '../styled-components-css/styles.store';
+import {
   SpanTextTitleSC,
-  DivAddProduct,
-  SpanTextAddProdctSC,
-  ButtonAddProductSC,
   DivAddProductBox,
-  DivBoxItemsSC,
   DivInfoVendorBoxSC,
   DivButtonChangrInformSC,
-  UlifnoSC
-    
-  } from "../styled-components-css/styles.VendorProfile";
-const VendorProfile = () =>{
-    return (
-        <DivBoxNewProductsBigST>
-            <DivBoxItemsSC>
-            <MenuProductST> <GreenST>Главная</GreenST> <img src="/default-images/arrowGreen.svg"  /> Профиль </MenuProductST>
-            </DivBoxItemsSC>
-            <DivBoxNewProductsST>
-               
-            <SettingsShop/>
-            
-               
-                <DivAddProductBox>
-                
-                 <DivInfoVendorBoxSC><SpanTextTitleSC>Имя профиля  <DivButtonChangrInformSC>Изменить информацию</DivButtonChangrInformSC></SpanTextTitleSC>
-                 <UlifnoSC>
-                    <li>fffff</li>
-                    <li>ffffff</li>
-                    <li>fffffff</li>
-                    <li>fffff</li>
-                 </UlifnoSC>
-                
+  UlifnoSC,
+  DivStoreInfoStuffProfileSC,
+  DivItemsOptionsSC,
+  DivInnerContentSC,
+  DivTwoSidesSC,
+  H1DefinSC,
+  NameNCityFieldSC,
+  BirthFieldSC,
+  ButtonBannerSC,
+  ButtonImgSC,
+} from '../styled-components-css/styles.VendorProfile';
+const CustomPropsBreadcrumb = ({ someProp }) => <span>{someProp}</span>;
+const routes = [
+  {
+    path: '/custom-props',
+    breadcrumb: CustomPropsBreadcrumb,
+    props: { someProp: 'Hi' },
+  },
+];
+const VendorProfile = () => {
+  const [indexSelectedButton, getIndexButton] = useState(0);
+  const onClickTab = (index) => (e) => {
+    getIndexButton(index);
+  };
+  const breadcrumbs = useReactRouterBreadcrumbs(routes);
+  const listContent = [
+    {
+      page: (
+        <DivStoreRightPanelSC>
+          <DivAddProductBox>
+            <DivInfoVendorBoxSC>
+              <SpanTextTitleSC>
+                Имя профиля{' '}
+                <DivButtonChangrInformSC onClick={onClickTab(1)}>Изменить информацию</DivButtonChangrInformSC>
+              </SpanTextTitleSC>
+              <UlifnoSC>
+                <li>fffff</li>
+                <li>ffffff</li>
+                <li>fffffff</li>
+                <li>fffff</li>
+              </UlifnoSC>
+            </DivInfoVendorBoxSC>
+            <DivInfoVendorBoxSC>FFFFFF</DivInfoVendorBoxSC>
+          </DivAddProductBox>
+        </DivStoreRightPanelSC>
+      ),
+    },
+    {
+      page: (
+        <DivStoreRightPanelSC>
+          <DivStoreInfoStuffProfileSC>
+            <H1BoldTextSC>Мой профиль</H1BoldTextSC>
+          </DivStoreInfoStuffProfileSC>
+          <DivItemsOptionsSC>
+            <DivInnerContentSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Баннер магазина</H1DefinSC><ButtonBannerSC>Загрузить баннер</ButtonBannerSC>
+              </DivTwoSidesSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Фото</H1DefinSC><ButtonImgSC>Загрузить фото</ButtonImgSC>
+              </DivTwoSidesSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Имя / название</H1DefinSC>
+                <NameNCityFieldSC label="email" type="email" placeholder="Фамилия  имя" />
+              </DivTwoSidesSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Обращение</H1DefinSC><label><input type="radio" checked name="dva"/> описание1</label> <label><input type="radio" name="dva"/> описание2</label>
+              </DivTwoSidesSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Дата рождения</H1DefinSC>  <BirthFieldSC label="email" type="email" placeholder="дд.мм.гг" />
+              </DivTwoSidesSC>
+              <DivTwoSidesSC>
+                <H1DefinSC>Место проживания</H1DefinSC>
+                <NameNCityFieldSC label="email" type="email" placeholder="Красноярский край, Россия" />
+              </DivTwoSidesSC>
+            </DivInnerContentSC>
+          </DivItemsOptionsSC>
+        </DivStoreRightPanelSC>
+      ),
+    },
+  ];
 
-                    
-                
-                    
-               
-                        
-                        </DivInfoVendorBoxSC>
-                 <DivInfoVendorBoxSC>FFFFFF</DivInfoVendorBoxSC>
-                </DivAddProductBox>
-                
-
-        </DivBoxNewProductsST>
-
-        </DivBoxNewProductsBigST>
-        
-
-        
-
-    )
-    
-}
+  return (
+    <>
+      <DivWrapLinkSC>
+        <DivHistorySC>
+          <DivBackBoxSC>
+            <FaArrowLeft color={'#85CB33'} size={15} />
+            <NavLinkSC iscolor={true} to={'/'}>
+              Назад
+            </NavLinkSC>
+          </DivBackBoxSC>
+          <div>
+            {breadcrumbs.map(({ match, breadcrumb }) => (
+              <span key={match.pathname}>
+                <NavLinkSC to={match.pathname}>{breadcrumb} / </NavLinkSC>
+              </span>
+            ))}
+          </div>
+        </DivHistorySC>
+      </DivWrapLinkSC>
+      <DivStoreWrapSC>
+        <DivStoreLeftPanelSC>
+          <DivStoreInfoSC>
+            <DivStoreInfoImageSC src={'/default-images/plant.jpg'}></DivStoreInfoImageSC>
+            <DivStoreInfoDataSC>
+              <StoreInfoHeaderSC>Название магазина</StoreInfoHeaderSC>
+              <StoreInfoSubHeaderSC>Город</StoreInfoSubHeaderSC>
+            </DivStoreInfoDataSC>
+          </DivStoreInfoSC>
+          <DivStoreOptionsSC>
+            <DivStoreOptionsLinkStoreSC to="/VendorProfile">Профиль</DivStoreOptionsLinkStoreSC>
+            <br />
+            <DivStoreOptionsLinkSC to="/sellersettings">Настройки</DivStoreOptionsLinkSC>
+            <br />
+            <HrLinkSC />
+            <DivStoreOptionsLinkSC to="/store/1">Магазин 50</DivStoreOptionsLinkSC>
+            <br />
+            <DivStoreOptionsLinkSC to="/">Оплата и доставка</DivStoreOptionsLinkSC>
+            <br />
+            <HrLinkSC />
+            <DivStoreOptionsLinkSC to="/">Покупки</DivStoreOptionsLinkSC>
+            <br />
+            <DivStoreOptionsLinkSC to="/">Продажи</DivStoreOptionsLinkSC>
+            <br />
+            <DivStarsPanelSC>
+              <DivStoreOptionsLeftPanelSC>
+                <DivStoreOptionsLinkSC to="/">Отзывы 30</DivStoreOptionsLinkSC>
+                <br />
+              </DivStoreOptionsLeftPanelSC>
+              <DivStoreOptionsRightPanelSC>
+                <StarRating product={true} />
+              </DivStoreOptionsRightPanelSC>
+            </DivStarsPanelSC>
+          </DivStoreOptionsSC>
+        </DivStoreLeftPanelSC>
+        {listContent[indexSelectedButton].page}
+      </DivStoreWrapSC>
+    </>
+  );
+};
 export default VendorProfile;
--- EcoCraftsCityReact/src/styled-components-css/styles.VendorProfile.js
+++ EcoCraftsCityReact/src/styled-components-css/styles.VendorProfile.js
@@ -1,7 +1,4 @@
-
 import styled from "styled-components/macro";
-
-
 export const MenuProductST = styled.div`
 display:flex;
 font-family: 'Montserrat';
@@ -12,10 +9,8 @@ line-height: 24px;
 align-self: left;
 color: #3D3D3D;
 margin-top: 40px;
-
   @media (max-width: 480px) {
     display:none;
-
     }
 `;
 export const GreenST = styled.div`
@@ -28,9 +23,8 @@ export const DivBoxNewProductsST = styled.div`
   margin-top: 80px;
   justify-self: center;
   max-width: 1420px;
-  padding 0 30px;
+  padding: 0 30px;
   grid-column-gap: 40px;
-
   @media (max-width: 480px) {
     grid-template-columns: 1fr;
     width: 100%;
@@ -40,11 +34,8 @@ export const DivBoxNewProductsBigST = styled.div`
 width: 100%;
 display: grid;
 margin-bottom:30px;
-
 `;
-
 export const DivLogoST=styled.div`
-
 background: #D9D9D9;
 border-radius: 12px;
 width: 102px;
@@ -54,15 +45,11 @@ display:flex;
 align-items: center;
 text-align:center;
 font-size:10px;
-
-
 `;
 export const DivInfoStoreST=styled.div`
 display: grid;
   grid-template-columns: 1fr 1fr;
-
   font-size:15px;
-
   background: rgba(243, 243, 243, 0.5);
   border-radius: 20px;
   padding: 20px;
@@ -74,13 +61,8 @@ display: grid;
     border:none;
     padding: 0px;
     margin: 0px;
-
-      }
-  
- 
-
+  }
 `;
-
 export const MarginST=styled.div`
 margin-top:10px;
 font-size:10px;
@@ -93,12 +75,8 @@ color: #000000;
 export const DivInfoST=styled.div`
 display:flex;
 flex-direction: column;
-
-
-
 `;
 export const SecondMenuST=styled.div`
-
 background: rgba(243, 243, 243, 0.5);
 border-radius: 20px;
 margin-top:10px;
@@ -107,19 +85,15 @@ height: 230px;
 margin-bottom:30px;
 display:flex;
 flex-direction: column;
-
 @media (max-width: 1100px) {
   width: 270px;
-
 }
 @media (max-width: 480px) {
   display:none;
   border:none;
   }
 `;
-
 export const DivStoreSetingST=styled.div`
-
 margin-left:20px;
 margin-top:10px;
 font-weight: 500;
@@ -127,20 +101,12 @@ font-family: 'Montserrat';
 font-style: normal;
 font-size: 15px;
 line-height: 18px;
-
-
 `;
 export const TextBold=styled.div`
-
 font-weight:bold;
-
-
 `;
 export const Margin5pxST=styled.div`
-
 margin-top:10px;
-
-
 `;
 export const SpanTextTitleSC = styled.span`
 font-family: 'Montserrat';
@@ -158,17 +124,13 @@ margin-right:10px;
 export const UlifnoSC=styled.ul`
 display:flex;
 margin-left:10px;
-
-
 `;
-
 export const SpanTextTitleShopSC = styled.span`
 font-family: 'Montserrat';
 font-style: normal;
 font-weight: 600;
 font-size: 15px;
 line-height: 18px;
-
 color: #000000;
 `;
 export const SpanLogoText = styled.span`
@@ -178,24 +140,19 @@ font-weight: 600;
 font-size: 10px;
 line-height: 12px;
 text-align: center;
-
 color: #000000;
 `;
 export const DivAddProduct = styled.div`
 display:grid;
     justify-self: center;
-
     justify-content:center;
     align-self: center;
     align-items: center;
     text-align: center;
     margin-top:180px;
-    
     width:100%;
-
     @media (max-width: 1000px) {
       margin-top:100px;
-
       }
 `;
 export const SpanTextAddProdctSC = styled.span`
@@ -206,16 +163,13 @@ font-size: 15px;
 line-height: 18px;
 color: #000000;
 position: relative;
-
 `;
 export const liInfoSC = styled.li`
 font-family: 'Montserrat';
 font-style: normal;
 font-size: 20px;
 margin-bottom:14px;
-
 `;
-
 export const ButtonAddProductSC = styled.button`
 font-family: 'Montserrat';
 font-style: normal;
@@ -231,7 +185,6 @@ border:none;
 cursor: pointer;
 margin-top:45px;
 `;
-
 export const DivAddProductBox = styled.div`
 display:flex;
 flex-direction: column;
@@ -248,28 +201,179 @@ align-items: center;
 padding: 2px 10px;
 color: #FFFFFF;
 font-size:16px;
+cursor: pointer;
 `;
 export const DivInfoVendorBoxSC = styled.div`
 background: rgba(243, 243, 243, 0.5);
 border-radius: 20px;
-
 `;
 export const DivBorderBox = styled.div`
-
 @media (max-width: 480px) {
   display:none;
   border:none;
   padding: 0px;
   margin: 0px;
-
     }
 `;
-
 export const DivBoxItemsSC = styled.div`
 display: grid;
 width:100%;
-  justify-self: center;
-  max-width: 1420px;
-padding 0 30px;
+justify-self: center;
+max-width: 1420px;
+padding: 0 30px;
+`;
+export const DivStoreInfoStuffProfileSC = styled.div`
+  // border: 2px solid blue;
+  display: grid;
+  grid-template-columns: 230px auto auto auto;
+  width: auto;
+  /* grid-gap: 1vw; */
+  max-width: 1010px;
+  padding-bottom: 10px;
+  @media (max-width: 1470px) {
+    grid-template-columns: 230px 130px auto;
 
+  }
+  @media (max-width: 990px) {
+    grid-template-columns: 230px 20px auto;
+  }
+`;
+export const DivItemsOptionsSC = styled.div`
+  background: rgba(243, 243, 243, 0.5);
+  display: grid;
+  border-radius: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  height: 760px;
+  // border: 2px solid black;
+  max-width: 1140px;
+`;
+export const DivInnerContentSC = styled.div`
+  display: grid;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 20px;
+  height: 720px;
+ // border: 2px solid black;
+  grid-template-rows: 140px 210px 50px 50px 50px 50px;
+  max-width: 1140px;
+  gap: 20px;
+`;
+export const DivTwoSidesSC = styled.div`
+display: grid;
+grid-template-columns: 240px auto;
+//border: 2px solid blue;
+`;
+export const H1DefinSC = styled.h1`
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 20px;
+line-height: 24px;
+color: #3D3D3D;
+justify-self: start;
+align-self: flex-start;
+margin-top: 5px;
+`;
+export const NameNCityFieldSC = styled.input`
+box-sizing: border-box;
+height:30px;
+/* Auto layout */
+display: flex;   
+  width: 100%;
+  max-width: 565px;
+  //min-width: 320px;
+flex-direction: row;
+align-items: center;
+justify-self: start;
+padding: 18px 0px 18px 18px;
+gap: 294px;
+/* Акцент 70% */
+border: 1px solid rgba(0, 0, 0, 0.33);
+border-radius: 12px;
+::placeholder{
+    font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 20px;
+/* identical to box height */
+color: rgba(37, 37, 37, 0.7);
+}
+:hover{
+    border: 1px solid  #76b42e;
+}
+@media (max-width:700px) {
+  ::placeholder{
+ 
+font-size: 13px;
+
+}
+}
+`;
+export const BirthFieldSC = styled.input`
+
+box-sizing: border-box;
+height:30px;
+/* Auto layout */
+display: flex;   
+  width: 100%;
+  max-width: 212px;
+  //min-width: 320px;
+flex-direction: row;
+align-items: center;
+justify-self: start;
+padding: 18px 0px 18px 18px;
+gap: 294px;
+/* Акцент 70% */
+border: 1px solid rgba(0, 0, 0, 0.33);
+border-radius: 12px;
+::placeholder{
+    font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 20px;
+/* identical to box height */
+color: rgba(37, 37, 37, 0.7);
+}
+:hover{
+    border: 1px solid  #76b42e;
+}
+@media (max-width:700px) {
+  ::placeholder{
+ 
+font-size: 13px;
+}
+}
+`;
+export const ButtonBannerSC = styled.button`
+border-radius: 12px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 24px;
+text-align: center;
+color: rgba(0, 0, 0, 0.33);
+cursor: pointer;
+border: 1px solid rgba(0, 0, 0, 0.33);
+height: 126px;
+background: none;
+max-width: 670px;
+`;
+export const ButtonImgSC = styled.button`
+border-radius: 12px;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 24px;
+text-align: center;
+color: rgba(0, 0, 0, 0.33);
+cursor: pointer;
+border: 1px solid rgba(0, 0, 0, 0.33);
+height: 193px;
+background: none;
+max-width:193px;
 `;

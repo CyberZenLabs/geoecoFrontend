3e73ac5dcd482706681b4d22d5706fe89f597255
ilya <moroz2002ilya@gmail.com>
2022-08-09 20:29:05 +0700

    uparrow

--- EcoCraftsCityReact/src/pages/MainPage.jsx
+++ EcoCraftsCityReact/src/pages/MainPage.jsx
@@ -26,6 +26,7 @@ import {
     ProductCardButtonSC,
 } from '../styled-components-css/styled.mainpage';
 import ScrollUpButton, {CircleArrow} from "react-scroll-up-button";
+import { HiOutlineArrowUp } from 'react-icons/hi';
 import DivPhoto from '../img/MainPagePhotos.svg';
 import DivMasters from '../img/MainPagePeople.svg';
 import Twit from '../img/FacebookIcon.svg';
@@ -151,7 +152,7 @@ const MainPage = () => {
                     transitionProperty: 'opacity, right, boxShadow',
                 }}
                 style={{
-                    // backgroundColor: '#000',
+                    backgroundColor: '#fff',
                     borderRadius: '50%',
                     border: 'none',
                     height: 70,
@@ -164,13 +165,18 @@ const MainPage = () => {
                     cursor: 'pointer',
                     opacity: 0,
                     right: -75,
-                    boxShadow: '10px 10px 10px #000'
+                    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+                    display: 'grid',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '35px',
                 }}
             >
-                <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {/* <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="40" cy="40" r="40" fill="#FAFAFA"/>
                     <path d="M53.3251 38.3433L41.6584 26.6766C41.4365 26.4642 41.1748 26.2977 40.8884 26.1866C40.3203 25.9533 39.6831 25.9533 39.1151 26.1866C38.8286 26.2977 38.567 26.4642 38.3451 26.6766L26.6784 38.3433C26.4608 38.5609 26.2883 38.8191 26.1705 39.1034C26.0528 39.3876 25.9922 39.6923 25.9922 40C25.9922 40.6213 26.239 41.2173 26.6784 41.6566C27.1178 42.096 27.7137 42.3428 28.3351 42.3428C28.9564 42.3428 29.5524 42.096 29.9917 41.6566L37.6684 33.9566V51.6666C37.6684 52.2855 37.9142 52.879 38.3518 53.3165C38.7894 53.7541 39.3829 54 40.0017 54C40.6206 54 41.2141 53.7541 41.6516 53.3165C42.0892 52.879 42.3351 52.2855 42.3351 51.6666V33.9566L50.0117 41.6566C50.2286 41.8753 50.4867 42.0489 50.7711 42.1674C51.0554 42.2858 51.3604 42.3468 51.6684 42.3468C51.9764 42.3468 52.2814 42.2858 52.5657 42.1674C52.8501 42.0489 53.1082 41.8753 53.3251 41.6566C53.5438 41.4397 53.7174 41.1816 53.8358 40.8973C53.9543 40.613 54.0153 40.308 54.0153 40C54.0153 39.6919 53.9543 39.387 53.8358 39.1026C53.7174 38.8183 53.5438 38.5602 53.3251 38.3433Z" fill="#85CB33"/>
-                </svg>
+                </svg> */}
+                <HiOutlineArrowUp color='#85CB33'/>
             </ScrollUpButton>
         </>
     );

84944e60ddabdbfc5018918a501cb29f24c392a7
RuslanGordin <101972460+ruslanGordin@users.noreply.github.com>
2022-08-09 13:30:00 +0700

    сделал карусель отзывы наших клиентов

--- EcoCraftsCityReact/src/index.js
+++ EcoCraftsCityReact/src/index.js
@@ -9,29 +9,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // import pages
 
-import Home from "./pages/Home";
-import PersonalData from "./pages/PersonalData";
-import StoreNewProduct from "./pages/StoreNewProduct";
-import StoreNoAuth from "./pages/StoreNoAuth";
-import NavBar from "./components/NavBar";
-import  Cart  from "./pages/Cart";
-import Mooo from "./pages/AdminLogin";
-import ProductDetail from "./pages/ProductDetail";
-import AdminDashboard from "./pages/AdminDashboard";
-import Registration from "./pages/Registration";
-import SignIn from "./pages/SignIn";
-import StoreFront from "./pages/StoreFront";
+import Home from './pages/Home';
+import PersonalData from './pages/PersonalData';
+import StoreNewProduct from './pages/StoreNewProduct';
+import StoreNoAuth from './pages/StoreNoAuth';
+import NavBar from './components/NavBar';
+import Cart from './pages/Cart';
+import Mooo from './pages/AdminLogin';
+import ProductDetail from './pages/ProductDetail';
+import AdminDashboard from './pages/AdminDashboard';
+import Registration from './pages/Registration';
+import SignIn from './pages/SignIn';
+import StoreFront from './pages/StoreFront';
 import VendorProfile from './pages/VendorProfile';
 import MapBiding from './pages/MapBinding';
 import AddNewProduct from './pages/AddNewProduct';
 import ModalSendEmail from './components/ModalSendEmail';
 //components
 // import AdminForm from "./pages/AdminForm";
-import { ToastContainer } from "react-toastify";
-import AppContext, { AppProvider } from "./context/AppContext";
-import { AuthProvider } from "./context/AuthContext";
-import Reviews from "./pages/Reviews";
-import Page_404 from "./pages/404";
+import { ToastContainer } from 'react-toastify';
+import AppContext, { AppProvider } from './context/AppContext';
+import { AuthProvider } from './context/AuthContext';
+import Reviews from './pages/Reviews';
+import Page_404 from './pages/404';
 import Footer from './components/Footer';
 import Catalog from './components/Catalog';
 import ReviewForm from './components/ReviewForm';
@@ -40,12 +40,12 @@ import EcoModal from './components/Modal';
 import PassModal from './components/ModalPassChange';
 import MailModal from './components/ModalAddressChange';
 import SellerSettings from './pages/SellerSettings';
+import AboutUs from './pages/AboutUs';
 import Terms from './privacy/Terms';
 import Privacy from './privacy/Privacy';
 
 import Modalstore from './components/ModalRegStore';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 
 root.render(
   <>
@@ -56,14 +56,14 @@ root.render(
           <BrowserRouter>
             {/* Same as */}
             <Modalstore></Modalstore>
-            <ModalSendEmail/>
+            <ModalSendEmail />
             <EcoModal></EcoModal>
             <PassModal></PassModal>
             <MailModal></MailModal>
-            
+
             <div id="overlay-nav"></div>
             <NavBar />
-            
+
             <Catalog />
             <Routes>
               <Route index element={<Home />} />
@@ -72,6 +72,7 @@ root.render(
 
               <Route path="storenoauth" element={<StoreNoAuth />} />
               <Route path="addnewproduct" element={<AddNewProduct />} />
+              <Route path="about-us" element={<AboutUs />} />
 
               <Route path="vendorprofile" element={<VendorProfile />} />
 

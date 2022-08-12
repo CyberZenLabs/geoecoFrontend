6de0f825dacb86492484c378477abd8750eaa0df
ilya <moroz2002ilya@gmail.com>
2022-08-12 14:23:27 +0700

    buttons

--- EcoCraftsCityReact/src/styled-components-css/styled.carousel.jsx
+++ EcoCraftsCityReact/src/styled-components-css/styled.carousel.jsx
@@ -13,7 +13,8 @@ export const DivBoxCarouselSC = styled.div`
 export const DivContentCarouselSC = styled.div`
   //position: relative;
   //height: 400px;
-  width: 1322px;
+  width:900px;
+  column-gap: 100px;
   //overflow: auto;
   
 `;
@@ -21,9 +22,27 @@ export const DivContentCarouselSC = styled.div`
 export const ButtonCarouselSC = styled.button`
   //position: relative;
   //height: 400px;
-  width: 22px;
-  height: 22px;
-  background: #76b42e;
+  width: max-content;
+  height: max-content;
+  /* background: #76b42e; */
   //overflow: auto;
   
+`;
+
+export const DivPageButtonSC = styled.div`
+position: absolute;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  width: 45px;
+  height: 45px;
+  box-shadow: 0 2px 16px rgba(0, 0, 0, 0.11);
+  border-radius: 30px;
+  background-color: #ffffff;
+  z-index: 200;
+  font-size: 23px;
+  color: black;
+  :hover {
+    color: rgb(133, 203, 51);
+  }
 `;
